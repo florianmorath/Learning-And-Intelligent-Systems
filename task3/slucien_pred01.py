e34215ee837eaa9f361c1a83c5b76f2fc57eebf2
slucien <slucien@student.ethz.ch>
2017-05-01 11:30:00 +0200

    added changes of activation function to logistic

--- task3/slucien_pred01.py
+++ task3/slucien_pred01.py
@@ -14,7 +14,11 @@ df_test = pd.read_csv('test.csv', float_precision = 'round_trip')
 '''
 
 # prepare feature matrix X and response vector y
-feature_cols = ['x1','x2','x3','x4','x5','x6','x7','x8','x9','x10','x11','x12','x13','x14','x15']
+feature_cols = []
+for i in range(1,101):
+    feature_cols.append('x' + str(i))
+
+#feature_cols = ['x1','x2','x3','x4','x5','x6','x7','x8','x9','x10','x11','x12','x13','x14','x15']
 y_train = df_train['y']
 X_train = df_train[feature_cols]
 X_test = df_test[feature_cols]
@@ -24,39 +28,40 @@ X_test = df_test[feature_cols]
 alphas = 10 ** np.linspace(10,-5,100) * 0.5
 param_grid = [{'alpha': alphas}]
 
+'''
 # feature selection
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import f_classif,chi2,mutual_info_classif
 sel = SelectKBest(f_classif, k = 11)
-
+'''
 
 #clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes(5, 2), random_state=1)
-clf = MLPClassifier(alpha=1.4240179342178966e-05)
-
-sel_clf = make_pipeline(sel, clf)
+clf = MLPClassifier(alpha=1.4240179342178966e-05, activation = 'logistic')
 '''
+sel_clf = make_pipeline(sel, clf)
+
 from sklearn.model_selection import GridSearchCV
 grid_search = GridSearchCV(estimator = clf, param_grid =  param_grid, cv = 10, scoring = 'accuracy')
 grid_search.fit(X_train, y_train)
 print('grid search best params = ', grid_search.best_params_)
 '''
-sel_clf.fit(X_train, y_train)
+clf.fit(X_train, y_train)
 
 
 # print out cross validation mean score for the chosen model
 from sklearn.model_selection import cross_val_score
-scores = cross_val_score(sel_clf, X_train, y_train, cv = 10, scoring = 'accuracy')
+scores = cross_val_score(clf, X_train, y_train, cv = 10, scoring = 'accuracy')
 print('cross val mean score = ', scores.mean())
 
 
 # print accuracy
 from sklearn.metrics import accuracy_score
-accuracy_train = accuracy_score(y_train, sel_clf.predict(X_train))
+accuracy_train = accuracy_score(y_train, clf.predict(X_train))
 print('Accuracy on training data = ', accuracy_train)
 
 # predict values on test data
-y_test = sel_clf.predict(X_test)
+y_test = clf.predict(X_test)
 
 # write predictions to csv file
 id = []
